144bf716558fbe2ff065a10cfda3860ad3912a8a
Jack McGrath <jack_mcgrath@jack-mcgrath.local>
2015-06-05 17:11:53 -0700

    new internsgit add .

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -1,19 +1,24 @@
 function main(){
 var choice;
-var a=0;
+var right=false;
 choice=prompt("What's your name?");
 choice=choice.toUpperCase();
-var names = ["DALTON", "VIVIAN", "TESS", "JACK", "ANDREW", "LAWRENCE", "CARTER", "JESSICA", "CATHIE", "RASMUS", "KEVIN", "SIERRA", "ABBY", "NIKKI", "JACKSON", "MINA", "SARAH", "NATHAN", "MICHAEL", "EMILY", "NICOLAS"];
-  for (var i=0; i<names.length; ++i){
-    if (choice==names[i]){
-      alert("yes u r");
-      a++;
+var names2014 = ["DALTON", "VIVIAN", "TESS", "JACK", "ANDREW", "LAWRENCE", "CARTER", "JESSICA", "CATHIE", "RASMUS", "KEVIN", "SIERRA", "ABBY", "NIKKI", "JACKSON", "MINA", "SARAH", "NATHAN", "MICHAEL", "EMILY", "NICOLAS"];
+var names2015 = ["ANUPA", "ARTHUR", "CHARLES", "CHRISTINA", "CONG", "COREY", "DANIEL", "DAVID", "EVAN", "GABE", "GREG", "GREGORY", "IAIN", "JACK", "JACQLYN", "JESSICA", "JOEL", "JORDAN", "JULIA", "KEZIAH", "LINCOLN", "LINDA", "LIZ", "MACKENZIE", "MARC", "MAYA", "MELANIE", "NICO", "NICOLAS", "OLIVIA", "QI", "RAISSA", "ROB", "ROEE", "SAM", "SHREYAS", "SHU-HAN", "SOPHIA", "SUMEET", "TUSHAR", "WHITNEY", "YAUN", "ZHENG", "ZULSAR"]
+  for (var i=0; i<names2015.length; ++i){
+    if (choice==names2014[i]){
+      alert("yes u r (old fella)");
+      right=true;
+    }
+    if (choice==names2015[i]){
+      alert("yes u r!!!");
+      right=true;
     }
   }
-  if (a==0){
+  if (!right){
     alert("nope");
   }
-  if (a==1 && choice=="EMILY"){
+  if (right && choice=="EMILY"){
     alert(":) :) :)");
   }
 }
